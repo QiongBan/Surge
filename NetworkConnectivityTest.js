85148e48dbb1fb87f9470aef93d8b7ec93640c86
Bancheng <QiongBan@users.noreply.github.com>
2023-04-05 14:36:28 -0700

    Changed

--- NetworkConnectivityTest.js
+++ NetworkConnectivityTest.js
@@ -21,8 +21,7 @@ function http(req) {
     return new Promise((r) => {
 			let time = Date.now();
         $httpClient.post(req, (err, resp, data) => {
-            r(req.split(".")[1]+
-						'\xa0\xa0\xa0\xa0\xa0\t: ' + (Date.now() - time)+' ms');
+            r(req.split(".")[1]+ '\xa0\xa0\xa0\xa0\xa0\t: ' + (Date.now() - time)+' ms');
         });
     });
 }
