5bcb68efe03775c0adf1f95ee36d05d959069f43
Bancheng <QiongBan@users.noreply.github.com>
2023-04-05 14:43:25 -0700

    Changed

--- NetworkConnectivityTest.js
+++ NetworkConnectivityTest.js
@@ -3,11 +3,11 @@ Bilibili:'https://www.bilibili.com',
 Baidu:'https://www.baidu.com',
 Youtube:'https://www.youtube.com/',
 Google:'https://www.google.com/generate_204',
-Github:'https://www.github.com'
+𝐆𝐢𝐭𝐡𝐮𝐛:'https://www.github.com'
 }
 
 !(async () => {
-await Promise.all([http($.Baidu),http($.Biibili),http($.Github),http($.Google),http($.Youtube)]).then((x)=>{
+await Promise.all([http($.Baidu),http($.Biibili),http($.𝐆𝐢𝐭𝐡𝐮𝐛),http($.Google),http($.Youtube)]).then((x)=>{
 	$done({
     title: '𝐍𝐞𝐭𝐰𝐨𝐫𝐤 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐢𝐯𝐢𝐭𝐲 𝐓𝐞𝐬𝐭',
     content: x.join('\n'),
@@ -20,7 +20,7 @@ await Promise.all([http($.Baidu),http($.Biibili),http($.Github),http($.Google),h
 function http(req) {
     return new Promise((r) => {
 			let time = Date.now();
-        $httpClient.post(req, (err, resp, data) => {
+        $httpClient.post([req], (err, resp, data) => {
             r(req.split(".")[1]+ ':' + (Date.now() - time)+' ms');
         });
     });
